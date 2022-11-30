4b2bcdeee294144aa8c7fad8b87f63aaa7762c94
guyguy-nll <guyalexandren@gmail.com>
2022-11-30 17:04:06 +0100

    Widegt frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,71 +1,95 @@
-import React from 'react';
-import Button from './components/button';
-import Movie from './components/movie';
-import axios from 'axios';
-import { map } from 'lodash';
-import './App.css';
+import React from "react";
+import Button from "./components/button";
+import Movie from "./components/movie";
+import Search from "./components/search";
+import axios from "axios";
+import { map } from "lodash";
+import "./App.css";
 
 const BACKEND_BASE_URL = "http://localhost:3001/movies/";
 
 export default class App extends React.Component {
   constructor() {
     super();
-    
+
     this.state = {
       meteos: [],
-      addFilmInputValue: ''   
-    }
+      addFilmInputValue: "",
+    };
   }
 
   componentDidMount() {
     this.getList();
   }
 
-  addFilmInputChange = event => {
+  addFilmInputChange = (event) => {
     this.setState({ addFilmInputValue: event.target.value });
 
-    console.log('value is:', event.target.value);
+    console.log("value is:", event.target.value);
   };
-  
-  getList= () => {
-    axios.get(BACKEND_BASE_URL).then((data) => data.data && data.data.meteos && this.setState({ meteos: data.data.meteos }));
-  }
-  
-  addMovie= () => {
-    axios.put(BACKEND_BASE_URL, { meteo: this.state.addFilmInputValue }).then((data) => this.getList());
-  }
-  
-  deleteMovie= (id) => {
+
+  getList = () => {
+    axios
+      .get(BACKEND_BASE_URL)
+      .then(
+        (data) =>
+          data.data &&
+          data.data.meteos &&
+          this.setState({ meteos: data.data.meteos })
+      );
+  };
+
+  addMovie = () => {
+    axios
+      .put(BACKEND_BASE_URL, { meteo: this.state.addFilmInputValue })
+      .then((data) => this.getList());
+  };
+
+  deleteMovie = (id) => {
     axios.delete(`${BACKEND_BASE_URL}/${id}`).then((data) => this.getList());
-  }
-  
+  };
+
   renderCategory = (label, action) => {
-    return (<div className='category'>
-    <Button text={label} onClick={action} />
-    </div>)
-  }
-  
-  render() {  
-    const { meteos, addFilmInputValue} = this.state;
+    return (
+      <div className="category">
+        <Button text={label} onClick={action} />
+      </div>
+    );
+  };
+
+  render() {
+    const { meteos, addFilmInputValue } = this.state;
+    const handleOnSearchChange = (searchData) => {
+      const [lat, lon] = searchData.value.split(" ");
+    };
 
     return (
       <div>
-        {this.renderCategory('Refresh', this.getList)}
-        <div className='category'>
-              <input
-                type="text"
-                id="addFilm"
-                name="addFilm"
-                onChange={this.addFilmInputChange}
-                value={addFilmInputValue}
-              />
-            <Button text={'Add movie'} onClick={this.addMovie} />
+        {this.renderCategory("Refresh", this.getList)}
+        <div className="category">
+          <input
+            type="text"
+            id="addFilm"
+            name="addFilm"
+            onChange={this.addFilmInputChange}
+            value={addFilmInputValue}
+          />
+          <Search onSearchChange={handleOnSearchChange} />
+          <Button text={"Add movie"} onClick={this.addMovie} />
         </div>
 
-        <div className='movies'>
-          {map(meteos, (meteo, index) => <Movie key={`meteo-${index}`} infos={meteo} deleteMovie={this.deleteMovie}/>)}
+        <div id="body2">
+          <div className="movies">
+            {map(meteos, (meteo, index) => (
+              <Movie
+                key={`meteo-${index}`}
+                infos={meteo}
+                deleteMovie={this.deleteMovie}
+              />
+            ))}
+          </div>
         </div>
       </div>
-      )
-    }
+    );
   }
+}
---
+++ frontend/src/apigeo.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "38555169e6mshe19d64f77b7e291p14cc7ejsn1a7865f02542",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY =
+  "38555169e6mshe19d64f77b7e291p14cc7ejsn1a7865f02542";
