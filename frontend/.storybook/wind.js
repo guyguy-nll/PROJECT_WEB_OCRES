7f892082371fa916589f6a67fb1d5d98d0875689
Matthieu Gascon <matthieu.gascon@hotmail.fr>
2022-12-05 10:46:26 +0100

    ajout story book

---
+++ frontend/.storybook/wind.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./all.css";
+import { map, split } from "lodash";
+
+export default class Wind extends React.Component {
+  renderInfo(label, info) {
+    return (
+      <div>
+        <div>{label}</div>
+        <div>{info}</div>
+      </div>
+    );
+  }
+
+  renderInfos(label, infos) {
+    return (
+      <div>
+        <div>{label}</div>
+        <div>
+          {map(infos, (info) => (
+            <div key={`infoList-${info}`}>{info}</div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const { infos, deleteMovie } = this.props;
+    const { id, meteo, temp, humidite, vent, pression } = infos;
+
+    return (
+      <div>
+        <div>{this.renderInfo("Force du vent", vent)}</div>
+      </div>
+    );
+  }
+}
